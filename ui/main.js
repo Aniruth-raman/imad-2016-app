68969c1040a6bc3cb69c3941f33088c190bf3ce5
Aniruth-raman <aniruth.raman5@gmail.com>
2016-09-29 21:23:49 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,6 +3,7 @@ alert('Hi,I\'m Aniruth');
 var element=document.getElementById('main-text');
 element.innerHTML="newvalue";
 var img=document.getElementById("madi");
+var marginLeft=0;
 function moveright(){
     marginLeft+=1;
     img.style.marginLeft='marginLeft'+'px';
