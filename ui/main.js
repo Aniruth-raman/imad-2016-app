8ab3aa0076421025e1031791f373246430775a7f
Aniruth-raman <aniruth.raman5@gmail.com>
2016-09-29 19:02:54 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,3 +2,7 @@ console.log('Loaded!');
 alert('Hi,I\'m Aniruth');
 var element=document.getElementById('main-text');
 element.innerHTML="newvalue";
+var img=document.getElementById("madi");
+element.onclick=function(){
+    img.style.marginleft="100px";
+};
