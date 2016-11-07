2900d438f24b82b7b625f1764959a14868c203ce
Aniruth-raman <aniruth.raman5@gmail.com>
2016-11-08 02:20:18 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,42 +1,26 @@
-/*console.log('Loaded!');
-alert('Hi,I\'m Aniruth');
-var element=document.getElementById('main-text');
-element.innerHTML="newvalue";
-var img=document.getElementById("madi");
-var marginLeft=0;
-function moveright(){
-    marginLeft+=1;
-    img.style.marginLeft=marginLeft+'px';
-}
-img.onclick=function(){
-    var interval=setInterval(moveright,50);
-};*/
-var button=document.getElementById('counter');
-button.onclick=function(){
-    var request=new XMLHttpRequest();
-    request.onreadystatechange=function(){
-        if(request.readyState===XMLHttpRequest.DONE){
-            if(request.status===200){
-                var counter=request.responseText;
-                var span=document.getElementById('count');
-   span.innerHTML=counter.toString();   
-            }
-        }
-    };
-    
-    request.open('GET','http://aniruth-raman.imad.hasura-app.io/counter',true);
-    request.send(null);
+var submit=document.getElementById("submit_btn");
+submit.onlick=function(){
+  var request=new XMLHttpRequest();
+  
+  request.onreadystatechange=function(){
+      if(request.readyState===XMLHttpRequest.DONE)
+      {
+          if(request.status===200){
+          alert("Logged In Successfully");
+          }else if(request.status===403){
+              alert("Incorrect Username/Password");
+          }else if(request.status===500){
+              alert("Something Went wrong");
+          }
+          
+      }
 };
 
-var nameInput=document.getElementById("name");
-var name=nameInput.value;
-var submit=document.getElementById("submit..btn");
-submit.onlick=function(){
-  var names=['name1','name2','name3'];
-  var list='';
-  for(var i=0;i<names.length;i++){
-      list+='<li>'+names[i]+'</li>';
-  }
-  var ul=document.getElementById("namelist");
-  ul.innerHTML=list;
+var username=document.getElementById('username').value;
+var password=document.getElementById('password').value;
+console.log(username);
+console.log(password);
+request.open('POST',"http://aniruth-raman.imad.hasura-app.io/login",true);
+request.setRequestHeader("Content-type","application/json");
+request.send(JSON.stringify({username:username,password:password}));
 };
