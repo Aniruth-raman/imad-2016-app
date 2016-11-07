f2805f00d750a2c94fbef76b3171d90e86e36656
Aniruth-raman <aniruth.raman5@gmail.com>
2016-11-08 02:29:22 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -20,7 +20,7 @@ var username=document.getElementById('username').value;
 var password=document.getElementById('password').value;
 console.log(username);
 console.log(password);
-request.open('POST',"http://aniruth-raman.imad.hasura-app.io/login",true);
+request.open('POST',"http://aniruth-raman.imad.hasura-app.io/login");
 request.setRequestHeader("Content-type","application/json");
 request.send(JSON.stringify({username:username,password:password}));
 };
