ee8f8d33f3ecba14b40f4006c615017d583e66b5
Aniruth-raman <aniruth.raman5@gmail.com>
2016-11-08 21:58:20 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ var path = require('path');
 var Pool = require('pg').Pool;
 var crypto=require('crypto');
 var bodyParser=require('body-parser');
+var session=require('express-session');
 
 var config = {
   host: 'db.imad.hasura-app.io',
@@ -15,7 +16,11 @@ var config = {
 var app = express();
 app.use(morgan('combined'));
 app.use(bodyParser.json());
-
+app.use(session({
+    secret:'somerandomvalue',
+    cookie:{maxAge:1000*60*60*24*30}
+    
+}));
 function createTemplate (data) {
      var title = data.title;
      var date = data.date;
@@ -122,6 +127,7 @@ app.post('/login',function(req,res){
            var hashedPassword=hash(password,salt);
            if(hashedPassword===dbString)
            {
+               req.session.auth={userId:result.rows[0].id};
                res.send("Correct Credentials");
            }else{
        res.status(403).send("Username/Password is Invalid"); 
@@ -132,7 +138,17 @@ app.post('/login',function(req,res){
        }
     });
 });
-
+app.get('/check-login',function(req,res){
+   if(req.session&&req.session.auth&&req.session.auth.userId){
+       res.send("You are logged in"+req.session.auth.userId.toString());
+   }else{
+       res.send("User is not logged In");
+   }
+});
+app.get('/logout',function(req,res){
+   delete req.session.auth;
+   res.send("User Logged Out");
+});
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
