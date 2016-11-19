0d66077ddae91b3e1b4b47f078d0109f84490cb9
Aniruth-raman <aniruth.raman5@gmail.com>
2016-11-19 23:12:30 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -36,13 +36,13 @@ function createTemplate (data) {
           </title>
           <meta name="viewport" content="width=device-width, initial-scale=1" />
           <link href="/ui/style.css" rel="stylesheet" />
+          <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.6.3/css/font-awesome.min.css">
       </head> 
       <body background=/ui/bg.jpg>
           <div class="container">
               <div>
               <a href="/" title="Home" class="icon style2 fa-home"><i class="fa fa-home" aria-hidden="true"></i>
-</a>
-                //   <a href="/">Home</a>
+</a><a href="/">Home</a>
               </div>
               <hr/>
               <h3>
