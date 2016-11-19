75c483c95599c08fc02f075e63aa0019becb709b
Aniruth-raman <aniruth.raman5@gmail.com>
2016-11-19 11:35:28 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -8,12 +8,12 @@ function loadLoginForm () {
         <input type="submit" id="register_btn" value="Register" />
         `;
         
-    var d = document.getElementById('dummy');
-    d.onclick = function () {
+    // var d = document.getElementById('dummy');
+    // d.onclick = function () {
         document.getElementById('login_area').innerHTML = loginHtml;
-        };
-    var submit = d.getElementById('login_btn');
-        // var submit = document.getElementById('login_btn');
+        // };
+    // var submit = d.getElementById('login_btn');
+         var submit = document.getElementById('login_btn');
     submit.onclick = function () {
         var request = new XMLHttpRequest();
         request.onreadystatechange = function () {
