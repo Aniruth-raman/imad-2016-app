dfd98b709bc55818048eefec2328e433f38e890a
Aniruth-raman <aniruth.raman5@gmail.com>
2016-11-24 17:15:07 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -89,6 +89,7 @@ function loadArticleForm() {
               if (request.status === 200) {
                   alert('Article created successfully');
                   store.value = 'Article Created';
+                  reload(true);
               } else {
                   alert('Article could not be created');
                   store.value = 'Create Article';
