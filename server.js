1ec5a79005b931ad568056bd431e18ba5f408fa9
Aniruth-raman <aniruth.raman5@gmail.com>
2016-11-24 15:18:31 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -97,6 +97,19 @@ app.post('/create-user', function (req, res) {
       }
    });
 });
+app.post('/create-article', function (req, res) {
+   var title = req.body.title;
+   var heading = req.body.heading;
+   var content = req.body.conntent;
+   pool.query('INSERT INTO "article" (title, heading, content) VALUES ($1, $2, $3)', [title, heading, content], function (err, result) {
+      if (err) {
+          res.status(500).send(err.toString());
+      } else {
+          res.send('Article successfully created: '+ heading);
+      }
+   });
+});
+
 app.post('/login', function (req, res) {
    var username = req.body.username;
    var password = req.body.password;
