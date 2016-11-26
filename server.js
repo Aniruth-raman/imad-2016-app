6001c4d5c5c5f4c64fc9cc6e776f04ef19d52be8
Aniruth-raman <aniruth.raman5@gmail.com>
2016-11-26 23:26:55 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -95,6 +95,10 @@ app.post('/create-user', function (req, res) {
    var password = req.body.password;
    var salt = crypto.randomBytes(128).toString('hex');
    var dbString = hash(password, salt);
+   if(username.trim()===''||password.trim()===''){
+       alert( Username/password cannot be blank);
+       return;
+   }
    pool.query('INSERT INTO "user" (username, password) VALUES ($1, $2)', [username, dbString], function (err, result) {
       if (err) {
           res.status(500).send(err.toString());
@@ -123,6 +127,10 @@ res.status(403).send('Register/Login to create new Article');
 app.post('/login', function (req, res) {
    var username = req.body.username;
    var password = req.body.password;
+   if(username.trim()===''||password.trim()===''){
+       alert("Username/Password cannot be blank");
+       return;
+   }
    pool.query('SELECT * FROM "user" WHERE username = $1', [username], function (err, result) {
       if (err) {
           res.status(500).send(err.toString());
